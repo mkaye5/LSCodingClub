2d0693a24aeb14201168cc2b05bc28b066514afd
Megan Kaye <megan.kaye38@gmail.com>
2017-11-30 22:26:14 -0500

    edited file

--- ls_coding_club_tkinter_tutorial_STUDENTS/__init__.py
+++ ls_coding_club_tkinter_tutorial_STUDENTS/__init__.py
@@ -3,15 +3,16 @@ from character.character import Character
 
 root = tk.Tk()
 
-C = tk.Canvas(root, bg="blue", height=500, relief="sunken", width=600)
+'''edit the code below'''
 
-coords = 10, 50, 240, 210
+C = tk.Canvas(root, bg='''any background color you want''', height='''pick one''', relief="sunken", width='''pick one''')
+coords = #where you want the character to start
+
+''''don't touch this code'''
 chara = Character(C, coords)
-
 frame = tk.Frame(root, width=100, height=100)
 frame.bind("<KeyPress>", chara.key)
 frame.pack()
 frame.focus_set()
-
 C.pack()
 root.mainloop()
